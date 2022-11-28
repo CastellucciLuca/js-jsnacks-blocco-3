925a1e4c78b7bbff1245cd91d5a7d5580f54c11d
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-28 16:27:40 +0100

    snack-1

--- js1/script.js
+++ js1/script.js
@@ -1,5 +1,4 @@
 // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
-
 let userNumber = 0;
 //Chiedo all'utente compreso dall'1 al 10 **Se il numero non è compreso riappare la stringa**
 while((userNumber < 1) || (userNumber > 10)){
