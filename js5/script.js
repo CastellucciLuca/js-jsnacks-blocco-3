7e76f2326e4c6ef8172501c8109da1e89c136b2e
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-29 12:07:43 +0100

    snack5

---
+++ js5/script.js
@@ -0,0 +1,10 @@
+/**
+* Crea una funzione che accetti due argomenti, e che restituisca un valore numerico randomico tra i due argomenti inclusi.
+*/
+function randomNumberBetweenLimits (limitMin, limitMax) {
+    const generatedNumber = Math.floor(Math.random() * (limitMax - limitMin) + limitMin)
+
+    return generatedNumber;
+}
+
+console.log(randomNumberBetweenLimits(1, 18));
