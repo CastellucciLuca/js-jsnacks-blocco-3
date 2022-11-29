155a2f4816c484572afbce9a3e42e3c0b93f86db
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-29 12:26:50 +0100

    Sesto-snack

---
+++ js6/script.js
@@ -0,0 +1,16 @@
+/**
+* Crea una funzione che dato un array, calcoli la somma dei suoi elementi.
+*/
+const array = ['Hello', 2, 55, 'Luca', 24];
+
+function sumArray (array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if( !(Number.isNaN( parseInt(array[i], 10)) ) ) {
+            sum += array[i];
+        }
+    }
+    console.log(sum);
+}
+
+sumArray(array);
