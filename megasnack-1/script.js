ffb60b6cd43e0831377ca3d70f2085a8e4a53639
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-30 11:44:11 +0100

    megasnack-1

--- megasnack-1/script.js
+++ megasnack-1/script.js
@@ -7,11 +7,32 @@
 *
 *
 */
-
-const array = [1,33,55,64,33,22,10,44];
 // funzione che crei un nuovo elemento <p> </p>
 // restituirlo a chi invoca la funzione
 //
 // ciclo per ogni elemento dell'array
 // se l'elemento è un numero pari, crei nuovo elemento e lo aggiungo al div verde
 // se l'elemento è un numero dispari, creo  nuovo elemento e lo aggiungo al div rosso
+const array = [1, 33, 55, 64, 33, 22, 10, 44];
+
+const odd = document.getElementById("red");
+const even = document.getElementById("green");
+
+function createElement(){
+
+    pElement = document.createElement("p");
+    return pElement;
+}
+
+for(let i = 0; i < array.length; i++){
+    
+    let p = createElement();
+    
+    if(array[i] % 2 == 1){
+        odd.append(p);
+        p.append(array[i]);
+    }else{
+        even.append(p);
+        p.append(array[i])
+    }
+}
